58483215c52d594979d3bfaaff4fd7588f25d05c
ccFguez <rennauto878@gmail.com>
2022-07-04 21:33:43 +0200

    add styling with chakra ui and color mode (not working on chrome)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../lib/theme'
 import Main from '../components/layouts/main'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Main router={router}></Main>
       <Component {...pageProps} key={router.route} />
     </ChakraProvider>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,12 @@
+import { ColorModeScript } from '@chakra-ui/react'
+
 const Page = props => {
-  return <div> Hello World!</div>
+  return (
+    <div>
+      <ColorModeScript initialColorMode="dark" />
+      <div>Hello World!</div>
+    </div>
+  )
 }
 
 export default Page
