a8ab249ff2c0ad553cc2f6e1f858853bb671c349
ccFguez <rennauto878@gmail.com>
2022-07-05 20:35:32 +0200

    remove unnecessary stuff

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,6 @@
-import { ColorModeScript } from '@chakra-ui/react'
-
 const Page = props => {
   return (
     <div>
-      <ColorModeScript initialColorMode="dark" />
       <div>Hello World!</div>
     </div>
   )
